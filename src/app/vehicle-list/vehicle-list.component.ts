b67309374fe07366b64f7f9151481fa832f3d829
Isuru Jayasinghe <isuru.j@nikiniautomation.com>
2018-10-05 11:34:28 +0530

    Add new vehicle and realtime refresh

--- src/app/vehicle-list/vehicle-list.component.ts
+++ src/app/vehicle-list/vehicle-list.component.ts
@@ -8,6 +8,7 @@ import {MatDialog} from '@angular/material';
   styleUrls: ['./vehicle-list.component.scss']
 })
 export class VehicleListComponent implements OnInit {
+  interval: any;
   allVehicles: any[];
   constructor(
     private getVehicles:VehicleServiceService,
@@ -29,7 +30,13 @@ export class VehicleListComponent implements OnInit {
     this.getVehicles.getAllVehicles().subscribe(result=>{
       this.allVehicles = result;
     });
+    this.interval = setInterval(() => { 
+      this.getVehicles.getAllVehicles().subscribe(result=>{
+        this.allVehicles = result;
+      });
+  }, 1000);
   }
+  
 
 }
 

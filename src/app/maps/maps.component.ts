f63f6e8deca8e636ec1e0cd2e2de42991eb6f4e2
Vihanga Paranahewa <Vihanga@nikiniautomation.com>
2018-10-17 11:21:00 +0530

    configure map.ts to back-end data format

--- src/app/maps/maps.component.ts
+++ src/app/maps/maps.component.ts
@@ -13,6 +13,8 @@ export class MapsComponent implements OnInit {
     truckIcon: any;
 
     polylines = [
+        {imeiNumber: 356307045861738,
+            trackingData: [
         {lat: 6.8575136, lng: 79.9088256, speed: 2},
         {lat: 6.856712, lng: 79.9100736, speed: 46},
         {lat: 6.8532288, lng: 79.9154048, speed: 47},
@@ -26,40 +28,43 @@ export class MapsComponent implements OnInit {
         {lat: 6.8450528, lng: 79.935008, speed: 34},
         {lat: 6.8453928, lng: 79.9439296, speed: 32},
         {lat: 6.8460832, lng: 79.9465088, speed: 43},
-        {lat: 6.8437952, lng: 79.9568832, speed: 30},
-        {lat: 6.8434248, lng: 79.9602304, speed: 40},
-        {lat: 6.8418432, lng: 79.9632576, speed: 7},
-        {lat: 6.8409368, lng: 79.9661504, speed: 16},
-        {lat: 6.8393232, lng: 79.9720768, speed: 47},
-        {lat: 6.8394216, lng: 79.9748992, speed: 40},
-        {lat: 6.838888, lng: 79.97856, speed: 46},
-        {lat: 6.8384088, lng: 79.9807104, speed: 39},
-        {lat: 6.8375968, lng: 79.9840704, speed: 50},
-        {lat: 6.8386896, lng: 79.986464, speed: 42},
-        {lat: 6.8395912, lng: 79.9900288, speed: 50},
-        {lat: 6.8397648, lng: 79.9930816, speed: 35},
-        {lat: 6.8399632, lng: 79.9949632, speed: 27},
-        {lat: 6.8404968, lng: 79.9972672, speed: 26},
-        {lat: 6.8408984, lng: 79.9990848, speed: 27},
-        {lat: 6.8412464, lng: 80.002496, speed: 7},
-        {lat: 6.841308, lng: 80.0036928, speed: 25},
-        {lat: 6.841532, lng: 80.00496, speed: 8},
-        {lat: 6.8426768, lng: 80.0060864, speed: 19},
-        {lat: 6.8448936, lng: 80.0097664, speed: 42},
-        {lat: 6.8447152, lng: 80.0137088, speed: 21},
-        {lat: 6.8446552, lng: 80.0147264, speed: 10},
-        {lat: 6.8405832, lng: 80.0216448, speed: 5},
-
+        {lat: 6.8437952, lng: 79.9568832, speed: 30}
+        ]
+        },
+        {imeiNumber: 356307045863458,
+            trackingData: [{lat: 6.8434248, lng: 79.9602304, speed: 40},
+                {lat: 6.8418432, lng: 79.9632576, speed: 7},
+                {lat: 6.8409368, lng: 79.9661504, speed: 16},
+                {lat: 6.8393232, lng: 79.9720768, speed: 47},
+                {lat: 6.8394216, lng: 79.9748992, speed: 40},
+                {lat: 6.838888, lng: 79.97856, speed: 46},
+                {lat: 6.8384088, lng: 79.9807104, speed: 39},
+                {lat: 6.8375968, lng: 79.9840704, speed: 50},
+                {lat: 6.8386896, lng: 79.986464, speed: 42},
+                {lat: 6.8395912, lng: 79.9900288, speed: 50},
+                {lat: 6.8397648, lng: 79.9930816, speed: 35},
+                {lat: 6.8399632, lng: 79.9949632, speed: 27},
+                {lat: 6.8404968, lng: 79.9972672, speed: 26},
+                {lat: 6.8408984, lng: 79.9990848, speed: 27},
+                {lat: 6.8412464, lng: 80.002496, speed: 7},
+                {lat: 6.841308, lng: 80.0036928, speed: 25},
+                {lat: 6.841532, lng: 80.00496, speed: 8},
+                {lat: 6.8426768, lng: 80.0060864, speed: 19},
+                {lat: 6.8448936, lng: 80.0097664, speed: 42},
+                {lat: 6.8447152, lng: 80.0137088, speed: 21},
+                {lat: 6.8446552, lng: 80.0147264, speed: 10},
+                {lat: 6.8405832, lng: 80.0216448, speed: 5}
+            ]
+        }
     ];
-    polyline: any[];
 
   constructor() { }
 
   ngOnInit() {
-      this.polyline = this.rebuildPolylines();
+      this.rebuildPolylines();
   }
 
-      onChooseLocation(event) {
+      /*onChooseLocation(event) {
 
           const newMarker = {
               lat: event.coords.lat,
@@ -73,31 +78,27 @@ export class MapsComponent implements OnInit {
           console.log('Longitudes :' + this.lng);
           this.markers.push(newMarker);
 
-      }
+      }*/
 
     private rebuildPolylines() {
-        const polyline = [];
-        for (let i = 0; i < this.polylines.length; i++) {
-            polyline[i] = {
-                path: [],
-            };
-            polyline[i].path = this.polylines[i];
-        }
-        if (polyline !== []) {
-            if (polyline[polyline.length - 1].path.speed > 60) {
-                this.truckIcon = './src/assets/img/red-truck-front.png';
-            } else if (polyline[polyline.length - 1].path.speed > 5) {
-                this.truckIcon = './src/assets/img/green-truck-front.png';
-            } else {
-                this.truckIcon = './src/assets/img/yellow-truck-front.png';
+        if (this.polylines !== []) {
+            for (let i = 0; i < this.polylines.length; i++) {
+
+                if (this.polylines[i].trackingData[this.polylines[i].trackingData.length - 1].speed > 60) {
+                    this.truckIcon = './src/assets/img/red-truck-front.png';
+                } else if (this.polylines[i].trackingData[this.polylines[i].trackingData.length - 1].speed > 5) {
+                    this.truckIcon = './src/assets/img/green-truck-front.png';
+                } else {
+                    this.truckIcon = './src/assets/img/yellow-truck-front.png';
+                }
+                const endMarker = {
+                    lat: this.polylines[i].trackingData[this.polylines[i].trackingData.length - 1].lat,
+                    lng: this.polylines[i].trackingData[this.polylines[i].trackingData.length - 1].lng,
+                    truckIcon: this.truckIcon
+                };
+                this.markEndLocation(endMarker);
             }
-            const endMarker = {
-                lat: polyline[polyline.length - 1].path.lat,
-                lng: polyline[polyline.length - 1].path.lng
-            };
-            this.markEndLocation(endMarker);
         }
-        return polyline;
     }
 
 

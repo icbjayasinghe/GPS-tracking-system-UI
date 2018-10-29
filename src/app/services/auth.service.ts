74a818d075f9a2ac1ae38742629f886e8acea8c8
Isuru Jayasinghe <isuru.j@nikiniautomation.com>
2018-10-29 16:52:58 +0530

    logout conflict fix

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -5,6 +5,8 @@ import { map} from 'rxjs/operators';
 @Injectable()
 export class AuthService {
   user: any;
+  token:any;
+  profile: any;
 
   constructor(private http: Http) { }
 
@@ -14,6 +16,16 @@ export class AuthService {
     headers.append('Content-Type', 'application/json');
     const url = 'http://localhost:3000/login';
     return this.http.post(url, this.user, { headers: headers}).pipe(map(res => res.json()));
+    }
+
+    fetchToken(){
+      this.token =localStorage.getItem('token');
+    }
+
+    logout(){
+      this.token = null ;
+      this.user = null ;
+      localStorage.clear();
     }
 }
 
